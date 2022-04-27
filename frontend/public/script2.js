d0ffbea0d6b14b0209a9606b6c9d605b818cf408
AndreaHolecz <andreaholecz94@gmail.com>
2022-04-27 10:00:09 +0200

    i-dont-know-what-am-i-doing

---
+++ frontend/public/script2.js
@@ -0,0 +1,20 @@
+console.log(data)
+
+const allBeers = function (inner, id){
+    return `
+        <section id="${id}">${inner}</section>
+    `
+}
+
+const beerCardComponent = function (title, sub, text){
+    return `
+        <div class="card">
+        
+        </div>
+    `
+}
+
+const loadEvent = function (){
+    const rootElement = document.getElementById("root");
+}
+
